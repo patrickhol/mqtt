97d717fbb04102fd7f1a7161536b15b197104202
Patrick Holody <patryk.holody@gmail.com>
2019-09-11 20:33:49 +0200

    update

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express')
+const sslRedirect = require('heroku-ssl-redirect');
+const app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => {
-    res.render('pages/index')
-  })
-  .post('/', (req, res) => {
-    console.log(req.body)
+// enable ssl redirect
+app.use(sslRedirect());
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-    res.render(req.body)
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => {
+//     res.render('pages/index')
+//   })
+//   .post('/', (req, res) => {
+//     console.log(req.body)
+
+//     res.render(req.body)
+//   })
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
