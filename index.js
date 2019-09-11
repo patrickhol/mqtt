1f9dc02531d2fc85e32d6e6c64c99ad02787c27e
Patrick Holody <patryk.holody@gmail.com>
2019-09-11 20:28:20 +0200

    update

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => {
+    res.render('pages/index')
+  })
+  .post('/', (req, res) => {
+    console.log(req.body)
+
+    res.render(req.body)
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
