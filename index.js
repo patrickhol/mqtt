64b2444582beae9c7b817371b2d7383b76ec5249
Patrick Holody <patryk.holody@gmail.com>
2019-09-11 21:44:28 +0200

    update

--- index.js
+++ index.js
@@ -5,33 +5,25 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const bodyParser = require('body-parser');
+const mqttHandler = require('./mqtt-client');
 
-
-// enable ssl redirect
 app.use(sslRedirect());
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var mqttClient = new mqttHandler();
+mqttClient.connect();
+
 app.get('/', function (req, res) {
   res.send('hello world');
 });
 app.post('/', (req, res) => {
-  console.log(req.body)
 
-  res.send(req.body)
+  mqttClient.sendMessage(req.body.message);
+  res.status(200).send("Message sent to mqtt", req.body.message);
+  // console.log(req.body)
+
+  // res.send(req.body)
 })
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-
-
-  // express()
-  //   .use(express.static(path.join(__dirname, 'public')))
-  //   .set('views', path.join(__dirname, 'views'))
-  //   .set('view engine', 'ejs')
-  //   .get('/', (req, res) => {
-  //     res.render('pages/index')
-  //   })
-  // .post('/', (req, res) => {
-  //   console.log(req.body)
-
-  //   res.render(req.body)
-  // })
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
